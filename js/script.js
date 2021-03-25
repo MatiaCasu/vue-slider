f0e3334e8d976266d487e56f2ecd4b2baae71432
Matia <matia.casu@gmail.com>
2021-03-25 16:54:31 +0100

    Attribuite src immagini ad img html tramite vue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-
+var app = new Vue({
+  el: "#app",
+  data: {
+    srcImg: [
+      "https://via.placeholder.com/1/0000FF",
+      "https://via.placeholder.com/1/FF0000",
+      "https://via.placeholder.com/1/FFF000",
+    ],
+  }
+});
