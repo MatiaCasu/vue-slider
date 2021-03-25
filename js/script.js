d0263362971dbb5742353d3cd68f305e7b04f20d
Matia <matia.casu@gmail.com>
2021-03-25 17:51:33 +0100

    Ciclo completo indice di srcImg al click di prev/next

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 var app = new Vue({
   el: "#app",
   data: {
-    srcImg: [
-      "https://via.placeholder.com/1/0000FF",
-      "https://via.placeholder.com/1/FF0000",
-      "https://via.placeholder.com/1/FFF000",
-    ],
+    img:{
+      srcImg: [
+        "https://via.placeholder.com/1/0000FF",
+        "https://via.placeholder.com/1/FF0000",
+        "https://via.placeholder.com/1/FFF000",
+      ],
+      i: 0
+    }
+  },
+  methods: {
+    prev: function(){
+      this.img.i--;
+
+      if(this.img.i < 0){
+        this.img.i = this.img.srcImg.length - 1;
+      }
+    },
+    next: function(){
+      this.img.i++;
+
+      if(this.img.i === this.img.srcImg.length){
+        this.img.i = 0;
+      }
+    }
   }
+
 });
